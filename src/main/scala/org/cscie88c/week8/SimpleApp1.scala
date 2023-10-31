b6bc94067ef99770e2b368bf7474e3fe36cf4624
Shawnjoseph2001 <shawnjoseph2001@gmail.com>
2023-10-30 22:41:21 -0700

    Week 8

--- src/main/scala/org/cscie88c/week8/SimpleApp1.scala
+++ src/main/scala/org/cscie88c/week8/SimpleApp1.scala
@@ -1,19 +1,30 @@
 package org.cscie88c.week8
 
-import com.typesafe.scalalogging.LazyLogging
-import scala.util.{Try, Success, Failure}
+import com.typesafe.scalalogging.{LazyLogging, Logger}
+
+import scala.util.{Failure, Success, Try}
 import scala.io.Source
 
 // run using: sbt "runMain org.cscie88c.week8.SimpleApp1 <args>"
 object SimpleApp1 extends LazyLogging {
-  
+
   def lineStreamFromFile(fileName: String): Option[LazyList[String]] =
     Try {
       LazyList.from(Source.fromResource(fileName).getLines())
     }.toOption
-  
-  def monthLines(month: String)(lines: LazyList[String]): LazyList[String] = ???
-      
-  def main(args: Array[String]): Unit = ???
 
+  def monthLines(month: String)(lines: LazyList[String]): LazyList[String] =
+    lines.filter(_.contains(month))
+
+  def main(args: Array[String]): Unit = {
+    val month = "Jan"
+    if (args.length < 1)
+      println("Path to data file is required!")
+    else
+      lineStreamFromFile(args(0)) match {
+        case Some(a) =>
+          println(s"Transactions in ${args(0)}: ${monthLines(month)(a).length}")
+        case None => println(s"No Transactions found for $month")
+      }
+  }
 }
--- src/main/scala/org/cscie88c/week8/SimpleApp2.scala
+++ src/main/scala/org/cscie88c/week8/SimpleApp2.scala
@@ -1,23 +1,39 @@
 package org.cscie88c.week8
 
 import com.typesafe.scalalogging.LazyLogging
-import scala.util.{Try, Success, Failure}
+
+import scala.util.Try
 import scala.io.Source
-import org.cscie88c.config.{ConfigUtils}
+import org.cscie88c.config.ConfigUtils
 import pureconfig.generic.auto._
 
 // write case class here
 
 // run with: sbt "runMain org.cscie88c.week8.SimpleApp2"
-object SimpleApp2 extends LazyLogging{
-  
+object SimpleApp2 extends LazyLogging {
 
   def lineStreamFromFile(fileName: String): Option[LazyList[String]] =
     Try {
       LazyList.from(Source.fromResource(fileName).getLines())
     }.toOption
-  
-  def monthLines(month: String)(lines: LazyList[String]): LazyList[String] = ???
-      
-  def main(args: Array[String]): Unit = ???
+
+  def monthLines(month: String)(lines: LazyList[String]): LazyList[String] =
+    lines.filter(_.contains(month))
+
+  case class SimpleApp2Config(
+      fileName: String,
+      month: String
+  )
+
+  def main(args: Array[String]): Unit = {
+    val config =
+      ConfigUtils.loadAppConfig[SimpleApp2Config]("org.cscie88c.simple-app-2")
+    lineStreamFromFile(config.fileName) match {
+      case Some(a) =>
+        println(
+          s"Transactions in ${config.month}: ${monthLines(config.month)(a).length}"
+        )
+      case None => println(s"No Transactions found for ${config.month}")
+    }
+  }
 }
